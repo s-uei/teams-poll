e13f7f94d486ed23d34bad30ec1d46c9b63ffb76
s-uei <s-uei@users.noreply.github.com>
2025-11-25 10:25:37 +0900

    echo server

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-Deno.serve(() => new Response("Hello"));
+Deno.serve(async (req) => {
+  const text = await req.text();
+  console.log(text);
+  return new Response(
+    JSON.stringify({
+      type: "message",
+      text,
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+});
