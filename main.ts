6475d06ed795f810c3fa477e2dda78d069dd9b51
s-uei <s-uei@users.noreply.github.com>
2025-11-25 11:00:57 +0900

    ok

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
-Deno.serve(async (req) => {
-  const text = await req.text();
-  console.log(text);
-  return new Response(
-    JSON.stringify({
-      type: "message",
-      text,
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
+import { DOMParser } from "@b-fuze/deno-dom";
+
+const r = (obj: unknown) =>
+  new Response(JSON.stringify(obj), {
+    headers: {
+      "Content-Type": "application/json",
     },
-  );
+  });
+
+Deno.serve(async (req) => {
+  if (req.method !== "POST") {
+    return new Response("Use POST.", { status: 400 });
+  }
+  const html = (await req.json())?.attachments?.[0]?.content ?? "";
+  const doc = new DOMParser().parseFromString(html, "text/html");
+  const text = doc.body.textContent;
+  const [command, ...items] = text.trim().split(/\s+/);
+  if (items.length === 0) {
+    return r({ text: `使い方: ${command} a b c` });
+  }
+  const chosen = items[Math.floor(Math.random() * items.length)];
+  return r({ text: `選ばれたのは ${chosen} でした。` });
 });
